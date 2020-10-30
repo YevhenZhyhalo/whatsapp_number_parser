e7dcacac56b52bb7ea4b3147526f0ed86c7183c9
gekailovegeka <gekailovegeka@gmail.com>
2020-10-30 02:11:35 +0200

    eee

--- script.py
+++ script.py
@@ -1,33 +1,30 @@
 from selenium import webdriver
 import selenium.webdriver as webdriver
 import selenium.webdriver.support.ui as ui
-import os
-import time
-import csv
-import re
-
+import os,time,csv,re,datetime
+import urllib.request as url
+#variables
 fn = ['Name', 'Given Name', 'Phone 1 - Type', 'Phone 1 - Value']
+file_path  ='contacts-' +datetime.datetime.now(tz=None).strftime("%h-%d_%H:%M")+'.csv'
 
-
+#init app
 chromedriver = os.path.join(os.path.expanduser('~'),
                             'Downloads', 'chromedriver')
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 driver.get("http://web.whatsapp.com")
 wait = ui.WebDriverWait(driver, 20)
+input("Press enter after scanning QR.\n ")
 
-input("Enter any keyword after scanning QR code.\n ")
 
 
+
+#PEZDEC
 left_panel = driver.find_elements_by_class_name("_210SC")
-
-contacts = 1
-
-
-with open('conts.csv', mode='w') as csv_file:
+with open(file_path, mode='w+') as csv_file:
 	writer = csv.DictWriter(csv_file, fieldnames=fn)
 	writer.writeheader()
-	k = 0
+	
 	for panel in left_panel:
 		panel.click()
 		name = driver.find_element_by_xpath(
@@ -38,18 +35,41 @@ with open('conts.csv', mode='w') as csv_file:
 		contacts = panel_parsed.get_attribute('title')
 
 		if(contacts == 'данные группы'):  
-			time.sleep(2)# wait until group info load
+			print(name+" is parsing now")
+			group_path = 'Groups/'+name
+			try:
+				os.makedirs(group_path)
+			except Exception:
+				group_path = group_path+'_repeat'
+				name = name +'_repeat'
+				os.makedirs(group_path)
+
+
+			try:
+				url.urlretrieve(driver.find_element_by_xpath("//*[@id='main']/header/div[1]/div/img").get_attribute('src'),group_path+'/photo.jpg')
+			except Exception:
+				print("no photo")
+
+
+			time.sleep(2)# Write logic
 			panel_parsed = driver.find_element_by_xpath(
                 "//*[@id='main']/header/div[2]/div[2]/span")
 			contacts = panel_parsed.get_attribute('title').split(',')
 
 			i = 1
-			for number in contacts:
-				if('+' in number):
-					writer.writerow({fn[0]: name + str(i), fn[1]: name +str(i), fn[2]: 'Mobile', fn[3]: number})
-				i+=1
-				if(i==2):
-					print(name+" is parsing now")
+			with open(group_path+'/'+name+'.csv', mode='w+') as csv_group_file:
+				group_writer = csv.DictWriter(csv_group_file, fieldnames=['name','number'])
+				group_writer.writeheader()
+
+
+				for number in contacts:#bolshaya dira
+					group_writer.writerow({'name':name+str(i),'number':number})
+
+					if('+' in number):
+						writer.writerow({fn[0]: name + str(i), fn[1]: name +str(i), fn[2]: 'Mobile', fn[3]: number})
+					i+=1
+					
+
 
 		else:  # ?????? nuzhno li???
 			panel_parsed.click()
